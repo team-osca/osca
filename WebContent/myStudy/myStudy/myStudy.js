855ac07f2826204bcce595b8247c4498a21196ed
Anna-TheDev <dev.annasun@gmail.com>
2023-07-02 14:02:58 +0900

    내 스터디 js 추가, 게시글 쓰기 모달창 추가 js하기 전.

--- WebContent/myStudy/myStudy/myStudy.js
+++ WebContent/myStudy/myStudy/myStudy.js
@@ -1,7 +1,12 @@
-
+//클릭시 리스트 색 변경
 $(document).ready(function() {
     $('li').click(function() {
       $('li').removeClass('active');
       $(this).addClass('active');
     });
   });
+
+// 스터디 리스트 수에 따라 왼쪽 토탈 스터디 수 변경
+const ulCount = document.querySelectorAll('.studyListCount ul').length;
+const label1 = document.querySelector('.label_1');
+label1.textContent = ulCount.toString();
